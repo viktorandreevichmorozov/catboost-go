d581c1d4144806389cef939aab16f30adc73d365
芦康平 <roger@lukangping.local>
2019-10-05 11:19:17 +0800

    update CalcModelPrediction.

--- model.go
+++ model.go
@@ -6,20 +6,45 @@ package catboost
 #include <stdlib.h>
 #include <stdbool.h>
 #include <model_calcer_wrapper.h>
-static char** makeCharArray(int size) {
-        return calloc(sizeof(char*), size);
-}
 
-static void setArrayString(char **a, char *s, int n) {
-        a[n] = s;
+static char** makeCharArray(int size) {
+    return calloc(sizeof(char*), size);
 }
 
 static void freeCharArray(char **a, int size) {
-        int i;
-        for (i = 0; i < size; i++)
-                free(a[i]);
-        free(a);
+	int i;
+	for (i = 0; i < size; i++)
+		free(a[i]);
+	free(a);
 }
+
+static char*** makeCharArray2(int size) {
+	return calloc(sizeof(char**), size);
+}
+
+static void freeCharArray2(char ***a, int sizeX, int sizeY) {
+	int i;
+	for (i = 0; i < sizeX; i++)
+		freeCharArray(a[i], sizeY);
+	free(a);
+}
+
+static float** makeFloatArray(int size) {
+	return calloc(sizeof(float*), size);
+}
+
+static void setCharArray2(char ***a, char **s, int n){
+	a[n] = s;
+}
+
+static void setFloatArray(float **a, float *f, int n){
+	a[n] = f;
+}
+
+static void setCharArray(char **a, char *s, int n) {
+    a[n] = s;
+}
+
 */
 import "C"
 
@@ -32,14 +57,6 @@ func getError() error {
 	messageC := C.GetErrorString()
 	message := C.GoString(messageC)
 	return fmt.Errorf(message)
-}
-
-func makeCStringArrayPointer(array []string) **C.char {
-	cargs := C.makeCharArray(C.int(len(array)))
-	for i, s := range array {
-		C.setArrayString(cargs, C.CString(s), C.int(i))
-	}
-	return cargs
 }
 
 // Model is a wrapper over ModelCalcerHandler
@@ -74,27 +91,32 @@ func LoadFullModelFromFile(filename string) (*Model, error) {
 
 // CalcModelPrediction returns raw predictions for specified data points
 func (model *Model) CalcModelPrediction(floats [][]float32, floatLength int, cats [][]string, catLength int) ([]float64, error) {
+
 	nSamples := len(floats)
 	results := make([]float64, nSamples)
 
-	floatsC := make([]*C.float, nSamples)
+	floatsC := C.makeFloatArray(C.int(nSamples))
+	defer C.free(unsafe.Pointer(floatsC))
 	for i, v := range floats {
-		floatsC[i] = (*C.float)(&v[0])
+		C.setFloatArray(floatsC, (*C.float)(&v[0]), C.int(i))
 	}
 
-	catsC := make([]**C.char, nSamples)
-	for i, v := range cats {
-		pointer := makeCStringArrayPointer(v)
-		defer C.freeCharArray(pointer, C.int(len(v)))
-		catsC[i] = pointer
+	catsC := C.makeCharArray2(C.int(nSamples))
+	defer C.freeCharArray2(catsC, C.int(nSamples), C.int(catLength))
+	for i, cat := range cats {
+		catC := C.makeCharArray(C.int(len(cat)))
+		for i, c := range cat {
+			C.setCharArray(catC, C.CString(c), C.int(i))
+		}
+		C.setCharArray2(catsC, catC, C.int(i))
 	}
 
 	if !C.CalcModelPrediction(
 		model.Handler,
 		C.size_t(nSamples),
-		(**C.float)(&floatsC[0]),
+		floatsC,
 		C.size_t(floatLength),
-		(***C.char)(&catsC[0]),
+		catsC,
 		C.size_t(catLength),
 		(*C.double)(&results[0]),
 		C.size_t(nSamples),
